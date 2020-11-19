1c3b0338adf2e953e3d75c3dd74d3b907cc9a9df
Aditya Gupta <adi@cs.utexas.edu>
2020-11-18 20:55:12 -0600

    Started avl.h

--- kernel/avl.h
+++ kernel/avl.h
@@ -1,6 +1,35 @@
 /*
  * avl.h
  */
+#ifndef _avl_h_
+#define _avl_h_
 
+#include "stdint.h"
 
+template <typename T>
+class avl_node {
+    avl_node<T> *left;
+    avl_node<T> *right;
+    T data;
+    uint32_t height;
+};
 
+template <typename T>
+class AVL {
+private:
+    avl_node<T> *root;
+public:
+    AVL() : root(nullptr), size(0) {}
+    ~AVL() {}
+    AVL<T>& operator=(const AVL& rhs);
+
+    void insert(T val);
+    T remove(T val);
+    T best_fit(T val);
+    avl_node<T> rotate_right(avl_node<T> n);
+    avl_node<T> rotate_left_right(avl_node<T> n);
+    avl_node<T> rotate_left(avl_node<T> n);
+    avl_node<T> rotate_right_left(avl_node<T> n);
+}
+
+#endif
