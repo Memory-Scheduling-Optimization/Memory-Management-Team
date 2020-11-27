8f4852d4a3994ddebad8206099794c3ec6664568
Riya Danait <rdanait@cs.utexas.edu>
2020-11-27 15:06:35 -0600

    initial commit: copied over next fit stuff

--- kernel/heap.cc
+++ kernel/heap.cc
@@ -4,223 +4,314 @@
 #include "blocking_lock.h"
 #include "atomic.h"
 
-/* A first-fit heap */
+/***********************************/
+/* ASSUMPTIONS			   */
+/* 1) Next fit allocation          */
+/* 2) Explicit list of free blocks */
+/* 3) Coalescing done during free  */
+/***********************************/
 
+/*************************/
+/* Some Helper Functions */
+/*************************/
 
-namespace gheith {
-    
-static int *array;
-static int len;
-static int safe = 0;
-static int avail = 0;
-static BlockingLock *theLock = nullptr;
-
-void makeTaken(int i, int ints);
-void makeAvail(int i, int ints);
-
-int abs(int x) {
-    if (x < 0) return -x; else return x;
+//purely for alignment purposes
+//recall 1 word is 4 bytes, so conversionFactor = 4
+//we need to return the minimum number of words needed to satisfy the number of bytes
+inline int convertByteToWord(int bytes, int conversionFactor) {
+	if(bytes % conversionFactor == 0) {
+		//divisible by 4 bytes per word, we're good to go
+		return bytes / conversionFactor;
+	}
+	else {
+		//we need to round up
+		return (bytes / conversionFactor) + 1;
+	}
 }
 
-int size(int i) {
-    return abs(array[i]);
+//return abs|x|
+template<typename T>
+inline T abs(T v) {
+	return (v < 0) ? -v : v;
 }
 
-int headerFromFooter(int i) {
-    return i - size(i) + 1;
+//add value to ptr
+//return ptr casted as T*
+template<typename T>
+inline T* ptr_add(void* ptr, int value) {
+	return (T*) (((char*) ptr) + value);
 }
 
-int footerFromHeader(int i) {
-    return i + size(i) - 1;
-}
-    
-int sanity(int i) {
-    if (safe) {
-        if (i == 0) return 0;
-        if ((i < 0) || (i >= len)) {
-            Debug::panic("bad header index %d\n",i);
-            return i;
-        }
-        int footer = footerFromHeader(i);
-        if ((footer < 0) || (footer >= len)) {
-            Debug::panic("bad footer index %d\n",footer);
-            return i;
-        }
-        int hv = array[i];
-        int fv = array[footer];
-  
-        if (hv != fv) {
-            Debug::panic("bad block at %d, hv:%d fv:%d\n", i,hv,fv);
-            return i;
-        }
-    }
 
-    return i;
-}
+/***********************************/
+/* Global Variables and Structures */
+/***********************************/
 
-int left(int i) {
-    return sanity(headerFromFooter(i-1));
-}
+namespace rdanait {
 
-int right(int i) {
-    return sanity(i + size(i));
-}
+        static BlockingLock *theLock = nullptr;
 
-int next(int i) {
-    return sanity(array[i+1]);
-}
+	struct Footer {
+		int sizeOfNode;
+	};
 
-int prev(int i) {
-    return sanity(array[i+2]);
-}
+	struct Header {
+		//positive means allocated node, negative means free node
+		//size is in words
+		int sizeOfNode;
 
-void setNext(int i, int x) {
-    array[i+1] = x;
-}
+		//ptr to header of the previous free node
+		Header* prevNode;
+		//ptr to header of the next free node
+		Header* nextNode;
 
-void setPrev(int i, int x) {
-    array[i+2] = x;
-}
+		//return the ptr to header of the next node
+		//we just have to add to this node
+		inline Header* getRightHeader() {
+			return ptr_add<Header>(this, abs<int>(sizeOfNode) * 4);
+		}
 
-void remove(int i) {
-    int prevIndex = prev(i);
-    int nextIndex = next(i);
+		//return the ptr to the footer of this node
+		inline Footer* getFooter() {
+			return ptr_add<Footer>(getRightHeader(), -4);
+		}
 
-    if (prevIndex == 0) {
-        /* at head */
-        avail = nextIndex;
-    } else {
-        /* in the middle */
-        setNext(prevIndex,nextIndex);
-    }
-    if (nextIndex != 0) {
-        setPrev(nextIndex,prevIndex);
-    }
-}
+		//return the ptr to header of the previous node
+		//we just have to subtract from this node
+		inline Header* getLeftHeader() {
+			int sizeOfLeftNode = abs(ptr_add<Footer>(this, -4) -> sizeOfNode);
+			return ptr_add<Header>(this, -sizeOfLeftNode * 4);
+		}
 
-void makeAvail(int i, int ints) {
-    array[i] = ints;
-    array[footerFromHeader(i)] = ints;    
-    setNext(i,avail);
-    setPrev(i,0);
-    if (avail != 0) {
-        setPrev(avail,i);
-    }
-    avail = i;
-}
+		inline bool isThisNodeFree() {
+			return sizeOfNode < 0;
+		}
 
-void makeTaken(int i, int ints) {
-    array[i] = -ints;
-    array[footerFromHeader(i)] = -ints;    
-}
+	};
 
-int isAvail(int i) {
-    return array[i] > 0;
-}
+	//ptr to header of first free node in the list
+	Header* freeListPtr = nullptr;
+	//Next fit rover
+	Header* rover;
+	//ptr to start of the heap
+	void* heapPtr = nullptr;
+	//heap size
+	int heapSize = 0;
 
-int isTaken(int i) {
-    return array[i] < 0;
-}
+	/*************************/
+	/* More Helper Functions */
+	/*************************/
+
+	//Node to be removed is no longer free
+	void removeFreeNode(Header* nodeToRemove) {
+		//Looks like this node is the only node in the free list
+		if(nodeToRemove -> prevNode == nullptr && nodeToRemove -> nextNode == nullptr) {
+			//Just make the ptr to header of the first free node nullptr
+			freeListPtr = nullptr;
+		}
+		//Looks like this node is the last node in the free list
+		else if(nodeToRemove -> prevNode != nullptr && nodeToRemove -> nextNode == nullptr) {
+			//Remove the previous node's next reference
+			nodeToRemove -> prevNode -> nextNode = nullptr;
+		}
+		//Looks like this node is the first node in the free list, but not the only node
+		else if(nodeToRemove -> prevNode == nullptr && nodeToRemove -> nextNode != nullptr) {
+			//Make the ptr to header of the first node in the free list be the next node now
+			freeListPtr = nodeToRemove -> nextNode;
+			//Remove the next node's prev reference
+			nodeToRemove -> nextNode -> prevNode = nullptr;
+		}
+		//Looks like this node is in between two free nodes in the free list
+		else {
+			//set the next reference of the prev node to this node's next node
+			nodeToRemove -> prevNode -> nextNode = nodeToRemove -> nextNode;
+			//set the prev reference of the next node to this node's prev node
+			nodeToRemove -> nextNode -> prevNode = nodeToRemove -> prevNode;
+		}
+	}
+
+	//Current and next node (which exists) must be free
+	void combineWithRightNode(Header* nodePtr) {
+		//We first need to get the header of the right node
+		Header* nextNodeHeader = nodePtr -> getRightHeader();
+		//Now, we need to update the size of the new node
+		int newSize = nextNodeHeader -> sizeOfNode + nodePtr -> sizeOfNode;
+		nodePtr -> sizeOfNode = newSize;
+		//Don't forget to update the footer too!
+		nodePtr -> getFooter() -> sizeOfNode = newSize;
+		//Now we can remove the next node from the free node list
+		removeFreeNode(nextNodeHeader);
+	}
 };
 
-void heapInit(void* base, size_t bytes) {
-    using namespace gheith;
 
-    Debug::printf("| heap range 0x%x 0x%x\n",(uint32_t)base,(uint32_t)base+bytes);
+/******************/
+/* Heap Functions */
+/******************/
 
-    /* can't say new becasue we're initializing the heap */
-    array = (int*) base;
-    len = bytes / 4;
-    makeTaken(0,2);
-    makeAvail(2,len-4);
-    makeTaken(len-2,2);
+void heapInit(void* start, size_t bytes) {
+
+    using namespace rdanait;
+    heapPtr = start;
+    heapSize = bytes;
+    freeListPtr = (Header*) start;
+    //Get the size, indicate it is free with negative
+    freeListPtr -> sizeOfNode = -convertByteToWord(heapSize, 4);
+    //Init the prev and next nodes to nullptr
+    freeListPtr -> prevNode = nullptr;
+    freeListPtr -> nextNode = nullptr;
+    //Make sure to put the size in the footer as well
+    freeListPtr -> getFooter() -> sizeOfNode = freeListPtr -> sizeOfNode;
+    rover = freeListPtr;
     theLock = new BlockingLock();
 }
 
 void* malloc(size_t bytes) {
-    using namespace gheith;
-    //Debug::printf("malloc(%d)\n",bytes);
-    if (bytes == 0) return (void*) array;
 
-    int ints = ((bytes + 3) / 4) + 2;
-    if (ints < 4) ints = 4;
+    using namespace rdanait;
+    //What if bytes < 0 or bytes > heapSize? Just return nullptr
+	if ((int) bytes < 0 || (int) bytes > heapSize - 8) return nullptr;
 
-    LockGuardP g{theLock};
+	//If malloc(0), we want to return one past the highest possible address malloc can return 
+	if (bytes == 0) return (void*) (((char*) heapPtr) + heapSize);
 
-    void* res = 0;
+	LockGuardP g{theLock};
+	//Get the size in words
+	int words = convertByteToWord(bytes, 4);
+	if (words < 2) words = 2;
 
-    int mx = 0x7FFFFFFF;
-    int it = 0;
-
-    {
-        int countDown = 20;
-        int p = avail;
-        while (p != 0) {
-            if (!isAvail(p)) {
-                Debug::panic("block is not available in malloc %p\n",p);
-            }
-            int sz = size(p);
-
-            if (sz >= ints) {
-                if (sz < mx) {
-                    mx = sz;
-                    it = p;
-                }
-                countDown --;
-                if (countDown == 0) break;
-            }
-            p = next(p);
+	Header* oldRover = rover;
+	//need to search through the free list for a free node using next fit algorithm
+	//search from rover to end of list
+	while(rover != nullptr) {
+            if(abs(rover -> sizeOfNode) - 2 >= words) {
+                //Now we check how big it is, so we can see if we need to allocate the whole node
+		//because the user won't be able to tell the difference
+		if(abs(rover -> sizeOfNode) - 8 <= words) {
+	            //negate the size in the header and footer, since it is now allocated
+		    rover -> sizeOfNode *= -1;
+	            rover -> getFooter() -> sizeOfNode *= -1;
+		    //remove from the free nodes list
+		    removeFreeNode(rover);
+		    //return a ptr to the node
+		    return ptr_add<void>(rover, 4);
+		}
+		else {
+		    //Else, we should split this node up
+		    //we need to update the size of the free node now
+		    rover -> sizeOfNode = -(abs(rover -> sizeOfNode) - words - 2);
+		    //update the footer's size as well
+		    rover -> getFooter() -> sizeOfNode = rover -> sizeOfNode;
+		    //Right node header size needs to update
+		    rover -> getRightHeader() -> sizeOfNode = words + 2;
+		    //Right node footer size also needs to be updated
+		    rover -> getRightHeader() -> getFooter() -> sizeOfNode = words + 2;
+		    //return a ptr to new node
+		    return ptr_add<void>(rover -> getRightHeader(), 4);
+		}
+	    }
+    	    //This node doesn't have a large enough size, let's try the next one
+	    rover = rover -> nextNode;
         }
-    }
-
-    if (it != 0) {
-        remove(it);
-        int extra = mx - ints;
-        if (extra >= 4) {
-            makeTaken(it,ints);
-            makeAvail(it+ints,extra);
-        } else {
-            makeTaken(it,mx);
+	//If we couldn't find a node after the rover, search from beginning to rover
+        rover = freeListPtr;
+	while(rover != oldRover) {
+	    if(abs(rover -> sizeOfNode) - 2 >= words) {         
+                //Now we check how big it is, so we can see if we need to allocate the whole node
+                //because the user won't be able to tell the difference
+                if(abs(rover -> sizeOfNode) - 8 <= words) {
+                    //negate the size in the header and footer, since it is now allocated
+                    rover -> sizeOfNode *= -1;
+                    rover -> getFooter() -> sizeOfNode *= -1;
+                    //remove from the free nodes list
+                    removeFreeNode(rover);
+                    //return a ptr to the node
+                    return ptr_add<void>(rover, 4);
+                }       
+                else {  
+                    //Else, we should split this node up
+                    //we need to update the size of the free node now
+                    rover -> sizeOfNode = -(abs(rover -> sizeOfNode) - words - 2);
+                    //update the footer's size as well
+                    rover -> getFooter() -> sizeOfNode = rover -> sizeOfNode;
+                    //Right node header size needs to update
+                    rover -> getRightHeader() -> sizeOfNode = words + 2;
+                    //Right node footer size also needs to be updated
+                    rover -> getRightHeader() -> getFooter() -> sizeOfNode = words + 2;
+                    //return a ptr to new node
+                    return ptr_add<void>(rover -> getRightHeader(), 4);
+                }       
+            }   
+            //This node doesn't have a large enough size, let's try the next one
+            rover = rover -> nextNode;
         }
-        res = &array[it+1];
-    }
 
-    return res;
+        return nullptr;
 }
 
 void free(void* p) {
-    using namespace gheith;
-    if (p == 0) return;
-    if (p == (void*) array) return;
 
-    LockGuardP g{theLock};
+    using namespace rdanait;
 
-    int idx = ((((uintptr_t) p) - ((uintptr_t) array)) / 4) - 1;
-    sanity(idx);
-    if (!isTaken(idx)) {
-        Debug::panic("freeing free block, p:%x idx:%d\n",(uint32_t) p,(int32_t) idx);
-        return;
+    //If nullptr, don't need to do anything
+    if (p == nullptr) return;
+        //Or if we're trying to free an invalid address, say, as a result of malloc(0)
+	if (p == ((char*) heapPtr) + heapSize) return;
+    if (ptr_add<Header>(p, -4) -> isThisNodeFree()) return;
+        LockGuardP g{theLock};
+
+	Header* headerOfP = ptr_add<Header>(p, -4);
+	//We first need to make sure this node is indicated to be free in header and footer by negating
+	headerOfP -> sizeOfNode *= -1;
+    headerOfP -> getFooter() -> sizeOfNode *= -1;
+    //Now we can add this node to our free nodes list
+    //This will not be the only node in our free nodes list
+    if (freeListPtr != nullptr) {
+    	//Make the prev reference null
+    	headerOfP -> prevNode = nullptr;
+    	//Header's next reference needs to be the header ptr
+	    headerOfP -> nextNode = freeListPtr;
+	    //Beginning of the free nodes list
+	    freeListPtr = headerOfP;
+	    //Next node's prev reference needs to be this node
+	    headerOfP -> nextNode -> prevNode = headerOfP;
+
+    }
+    //Else this is the only in our free nodes list
+    else {
+    	//Make this the first node in the free list
+    	freeListPtr = headerOfP;
+    	//Make the prev reference null
+		headerOfP -> prevNode = nullptr;
+		//Make the next reference null
+		headerOfP -> nextNode = nullptr;
     }
 
-    int sz = size(idx);
-
-    int leftIndex = left(idx);
-    int rightIndex = right(idx);
-
-    if (isAvail(leftIndex)) {
-        remove(leftIndex);
-        idx = leftIndex;
-        sz += size(leftIndex);
+    //Now we want to coalesce some nodes if they are free
+    //We first coalesce with the right node if it exists and is free
+    if ((char*) (headerOfP -> getRightHeader()) < (((char*) heapPtr) + heapSize) && headerOfP -> getRightHeader() -> isThisNodeFree()) {
+		combineWithRightNode(headerOfP);
     }
-
-    if (isAvail(rightIndex)) {
-        remove(rightIndex);
-        sz += size(rightIndex);
+    //We coalesce with the left node if it exists and is free
+    if (((char*) headerOfP) > ((char*) heapPtr) && headerOfP -> getLeftHeader() -> isThisNodeFree()) {
+		combineWithRightNode(headerOfP -> getLeftHeader());
     }
+    //Let's check to make sure rover isn't in the middle of a coalesced block; if it is
+    //just set it to the beginning of the coalesced block
+    if((char*) rover > (char*) headerOfP && (char*) rover < (char*)(headerOfP -> getRightHeader())) rover = headerOfP;
 
-    makeAvail(idx,sz);
 }
 
+int spaceUnallocated() {
+    using namespace rdanait;
+    int totSpace = 0;
+    Header* currentFreeNode = freeListPtr;
+    while(currentFreeNode != nullptr && currentFreeNode -> isThisNodeFree()) {
+	totSpace += abs(currentFreeNode -> sizeOfNode);
+	currentFreeNode = currentFreeNode -> nextNode;
+    }
+    return totSpace;
+}
 
 /*****************/
 /* C++ operators */
--- kernel/kernel.cc
+++ kernel/kernel.cc
@@ -1,6 +1,5 @@
 #include "stdint.h"
 #include "debug.h"
-
 #include "shared.h"
 #include "threads.h"
 #include "ext2.h"
@@ -9,31 +8,196 @@
 #include "process.h"
 #include "barrier.h"
 #include "sys.h"
+#include "pit.h"
 
-const char* initName = "/sbin/init";
+static void* blockPointersTest1[100];
+static void* blockPointersTest2[1000];
+constexpr uint32_t BIG_MALLOCS = 235;
+struct BigChunkOfMemory {
+    uint8_t a;
+    uint16_t b;
+    uint8_t c;
+    uint32_t d;
+    uint64_t e;
+    uint8_t f;
+    uint16_t g;
+    void* h[100];
 
-void printFile(Shared<Node> file) {
-    if (file == nullptr) {
-	Debug::printf("file does not exist\n");
-	return;
-    }
-    auto buffer = new char[file->size_in_bytes()];
-    file->read_all(0, file->size_in_bytes(), buffer);
-    for (uint32_t i = 0; i < file->size_in_bytes(); i++)
-	Debug::printf("%c", buffer[i]);
-    delete[] buffer;
-}
+    struct LittleChunkOfMemory {
+        int8_t i;
+        uint64_t j;
+        int8_t k;
+    } l;
+
+    struct MoreGarbage {
+        int8_t m;
+        void* n[1000];
+        uint64_t o;
+    } p;
+};
+void doEfficiencyTest(void);
+void doLargeStressTest(void);
+void doSpeedTest(void);
+void memUtil(void);
 
 void kernelMain(void) {
-    {
-	auto ide = Shared<Ide>::make(1);
-	auto fs = Shared<Ext2>::make(ide);	
-	auto init = fs->open(fs->root, initName);
-	auto pcb = Shared<PCB>{new Process(fs)};
-	thread(pcb, [=]() mutable { SYS::exec(init, "init", 0); });
+
+    Debug::printf("\n");
+    Debug::printf("Start of simple stress/correctness tests\n");
+    Debug::printf("Test 1\n");
+
+    const int LOOPS = 10000;
+
+    for (int i = 0; i < LOOPS; i++) {
+
+	for (int i = 0; i < 100; i++) {
+            blockPointersTest1[i] = malloc(100);
+        }
+
+        for (int i = 0; i < 100; i += 2) {
+            free(blockPointersTest1[i]);
+        }
+
+        for (int i = 1; i < 100; i += 2) {
+            free(blockPointersTest1[i]);
+            void *p = malloc(LOOPS * 100);
+            if (p == 0) Debug::panic("*** Unable to free every alternate block\n");
+            else free(p);
+        }
     }
-    // Debug::printf("init exited with status %d\n",
-    // 		  pcb->process()->exit_status->get());
-    stop();
+
+    Debug::printf("*** Passed Test 1: freeing alternate blocks\n");
+
+    Debug::printf("Test 2\n");
+
+    for(int i = 0; i < 1000; i++) {
+	blockPointersTest2[i] = malloc(1);
+    }
+
+    for(int i = 0; i < 1000; i++) {
+	free(blockPointersTest2[i]);
+    }
+
+    void* p = malloc(10000);
+    if (p == 0) Debug::panic("*** Unable to allocate large block after freeing many small allocations\n");
+    else Debug::printf("*** Passed Test 2: allocating large block after freeing many small allocations\n");
+
+    Debug::printf("Test 3\n");
+
+    void* p2 = malloc(0);
+    if (p2 == 0) Debug::panic("*** Unable to malloc(0)\n");
+    else {
+	free(p2);
+	Debug::printf("*** Passed test 3: malloc(0) and free(0)\n");
+    }
+
+    Debug::printf("End of simple stress/correctness tests\n");
+
+    Debug::printf("\n");
+
+    Debug::printf("Start of efficiency test\n");
+    doEfficiencyTest();
+    Debug::printf("End of efficiency test\n");
+
+    Debug::printf("\n");
+
+    Debug::printf("Start of big malloc stress trest\n");
+    doLargeStressTest();
+    Debug::printf("End of big malloc stress test\n");
+
+    Debug::printf("\n");
+
+    Debug::printf("Start of speed test\n");
+    doSpeedTest();
+    Debug::printf("End of speed test\n");
+
+    Debug::printf("\n");
+
+    Debug::printf("Memory utilization calculation\n");
+    memUtil();
+
 }
+
+void doEfficiencyTest() {
+
+    constexpr uint32_t len = 10 * 1000;
+    void** arr = new void*[len];
+
+    //A bunch of large mallocs
+    constexpr size_t large_bytes = 1000;
+    for (uint32_t i = 0; i < len/3; ++i) {
+        arr[i] = malloc(large_bytes);
+    }
+
+    //Free up some of the large blocks
+    for (uint32_t i = 0; i < len/12; ++i) {
+        free(arr[i]);
+    }
+
+    //A bunch of relatively small mallocs
+    constexpr size_t small_bytes = 500;
+    for (uint32_t i = len/3; i < (len * 2)/3; ++ i) {
+        arr[i] = malloc(small_bytes);
+    }
+
+    //Some relatively big mallocs
+    auto medium_bytes = (large_bytes * 3) / 4;
+    for (uint32_t i = (len * 2)/3; i < len; ++ i) {
+        arr[i] = malloc(medium_bytes);
+    }
+
+    for (uint32_t i = 0; i < len; ++ i) {
+        free(arr[i]);
+    }
+
+    Debug::printf("*** Passed space efficiency test\n");
+
+}
+
+void doLargeStressTest() {
+
+    BigChunkOfMemory** arr = new BigChunkOfMemory*[BIG_MALLOCS];
+
+    for (uint32_t i = 0; i < BIG_MALLOCS; ++ i) {
+        arr[i] = new BigChunkOfMemory;
+    }
+
+    for (uint32_t i = 0; i <= BIG_MALLOCS / 2; ++ i) {
+        free(arr[i]);
+    }
+
+    for (uint32_t i = BIG_MALLOCS - 1; i > BIG_MALLOCS / 2; -- i) {
+        free(arr[i]);
+    }
+
+    free(arr);
+
+    Debug::printf("*** Passed big malloc stress test\n");
+
+}
+
+template <typename Work>
+uint32_t howLong(Work work) {
+    uint32_t start = Pit::jiffies;
+    work();
+    return (Pit::jiffies - start);
+}
+
+void doSpeedTest() {
+
+    //Speed test calls large stress test
+    auto x = howLong([] {
+	doLargeStressTest();
+    });
+
+    Debug::printf("*** Time taken for big malloc stress test: %d\n", x);
+
+}
+
+void memUtil() {
+    double heapSize = 5 * 1024 * 1024;
+    double totalAmountUnallocated = spaceUnallocated();
+    Debug::printf("*** Memory utilized = %f%%\n", ((heapSize - totalAmountUnallocated)/heapSize)*100);
+}
+
 
